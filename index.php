fec018e28cadd6e910742e74e5adee0237eaf87d
Manoj <manya14gcc@gmail.com>
2026-03-02 12:49:55 +0530

    new index file

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>  
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <div class="logo">
+                    <img src="img/logo.png">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="content">
+                <h1>Home</h1>
+            </div>
+        </div>
+    </body>
+</html>
